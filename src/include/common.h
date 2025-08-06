13ee7052735a0058ffdbd1e235f63c2dc5d89c28
xuesixi <yue191617@gmail.com>
2025-08-06 16:23:02 +0800

    DEBUG_ASSERT() macro

--- src/include/common.h
+++ src/include/common.h
@@ -1,3 +1,17 @@
 
 #define DEBUG_TRACE_EXECUTION
 #define DEBUG_IMPLEMENTATION_CHECK
+
+#define STRINGIFY_DETAIL(x) #x
+#define STRINGIFY(x) STRINGIFY_DETAIL(x)
+
+#ifdef DEBUG_IMPLEMENTATION_CHECK
+    #define DEBUG_ASSERT(condition, message) \
+    do { \
+        if (!(condition)) { \
+            implementation_error(__FILE__ ":" STRINGIFY(__LINE__) " - " message);  \
+        } \
+    } while (0)
+#else
+    #define DEBUG_ASSERT(condition, message)
+#endif
